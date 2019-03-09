9859428076ca03bd92cb7d2ccf4fd846fbd170ff
Igor William <igorwm9@gmail.com>
2019-03-08 21:37:48 -0300

    TabBar Added

--- src/components/TabBarMenu.js
+++ src/components/TabBarMenu.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar, Image} from 'react-native';
 import {TabBar} from 'react-native-tab-view';
 
 export default props => (
     <View style={{ backgroundColor: '#115E54', elevation: 10, marginBottom: 15}}>
         <StatusBar backgroundColor='#114044'/>
 
-        <View style={{justifyContent:'center', height:50}}>
-            <Text style={{color:'white', fontSize:20, marginLeft:20}}>WhatsApp Clone</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{justifyContent:'center', height:50}}>
+                <Text style={{color:'white', fontSize:20, marginLeft:20}}>WhatsApp Clone</Text>
+            </View>
+
+            <View style={{flexDirection: 'row', marginRight: 20}}>
+                <View style={{ width: 50, justifyContent: 'center'}}>
+                    <Image source={require('../imgs/adicionar-contato.png')}/>
+                </View>
+                <View style={{justifyContent: 'center'}}>
+                    <Text style={{ fontSize: 20, color: 'white'}}>Sair</Text>
+                </View>
+            </View>
         </View>
 
-        <TabBar {...props} 
+        <TabBar 
+            {...props} 
             style={{ backgroundColor: '#115R54' }}
             indicatorStyle={{ backgroundColor: 'white' }}
         />
